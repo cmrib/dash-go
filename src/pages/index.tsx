cff9764132936c84eae893c54639802028caace9
cmrib <ciceromirandar@gmail.com>
2022-04-01 14:37:05 -0300

    setup react-hook-form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
 import { Input } from '../components/Form/Input';
+import { SubmitHandler, useForm, useFormState } from 'react-hook-form';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 
 export default function SignIn() {
-  return (
+  const { register, handleSubmit, formState } = useForm();
 
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(values)
+  }
+
+  return (
     <Flex
       w="100vw"
       h="100vh"
@@ -19,17 +31,18 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         direction="column"
+        onSubmit={handleSubmit(handleSignIn)}
 
       >
 
         <Stack spacing="4" >
 
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input name="email" type="email" label="E-mail"   {...register('email')} />
+          <Input name="password" type="password" label="Senha" {...register('password')} />
 
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size='lg' >Entrar</Button>
+        <Button type="submit" mt="6" colorScheme="pink" size='lg' isLoading={formState.isSubmitting} >Entrar</Button>
 
       </Flex>
     </Flex>
