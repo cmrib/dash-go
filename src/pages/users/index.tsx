af22f20700d292fff78a9a022f5df5f93e10ce29
cmrib <ciceromirandar@gmail.com>
2022-04-08 09:35:31 -0300

    setup react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,23 @@
-import { Box, Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import Link from 'next/link';
-import { useEffect } from "react";
+import { useQuery } from 'react-query'
 
 export default function UserList() {
+
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json();
+        return data;
+    })
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
 
     return (
         <Box>
@@ -34,46 +35,57 @@ export default function UserList() {
                                 fontSize="sm"
                                 colorScheme="pink"
                                 leftIcon={<Icon as={RiAddLine} fontSize="20" />}>Criar novo</Button>
-
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width="8"> </Th>
-                            </Tr>
-                        </Thead>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink"></Checkbox>
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width="8"> </Th>
+                                    </Tr>
+                                </Thead>
 
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Cicero Ribeiro</Text>
-                                        <Text fontSize="sm" color="gray.300">ciceromirandar@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                                {isWideVersion && <Td >
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>Editar</Button>
-                                </Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink"></Checkbox>
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Cicero Ribeiro</Text>
+                                                <Text fontSize="sm" color="gray.300">ciceromirandar@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>04 de Abril, 2021</Td>}
+                                        {isWideVersion && <Td >
+                                            <Button
+                                                as="a"
+                                                size="sm"
+                                                fontSize="sm"
+                                                colorScheme="purple"
+                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>Editar</Button>
+                                        </Td>}
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
